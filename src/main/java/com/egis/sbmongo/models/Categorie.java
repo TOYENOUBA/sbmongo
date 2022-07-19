b165e63b614a64c69fc00740bff288b6a9298332
unknown <togyenoubarimtebaye@gmail.comTOYNOUBATOGYENOUBAtr7githubcd ..exitgit config --global user.name togyenoubarimtebaye@gmail.comgit config --global user.email togyenoubarimtebaye@gmail.com>
2022-07-19 15:29:38 +0200

    tp2 de Nosql

---
+++ src/main/java/com/egis/sbmongo/models/Categorie.java
@@ -0,0 +1,25 @@
+package com.egis.sbmongo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author TR7
+ */
+
+@Document("categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categorie {
+    @Id
+    private String id;
+    private String nom;
+    private String description;
+   
+}
---
+++ src/main/java/com/egis/sbmongo/service/CategorieService.java
@@ -0,0 +1,17 @@
+package com.egis.sbmongo.service;
+
+import com.egis.sbmongo.models.Categorie;
+
+import java.util.List;
+import java.util.Optional;
+/**
+ *
+ * @author TR7
+ */
+
+public interface CategorieService {
+    public Categorie saveCategorie(Categorie c);
+    public Optional<Categorie> findById(String id);
+    public List<Categorie> findAll();
+    public void deleteCategorie(String id);
+}
